0d53372a352f718be91c5f91cdb4e9657dcb5c26
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-30 22:45:26 +0100

    refactor: move query inline

--- src/constants/sql_commands.py
+++
@@ -1,5 +0,0 @@
-INSERT_VALUES_INTO_TABLE = """
-INSERT INTO 
-top_artist_popularities (artist_name, popularity, term)
-VALUES(?, ?, ?);
-"""
--- src/database/create_database.py
+++ src/database/create_database.py
@@ -1,9 +1,14 @@
 from .database import Database
-from ..constants import SQL_DATA, INSERT_VALUES_INTO_TABLE
+from ..constants import SQL_DATA
 
 
 def create_db(rows):
     """Creates SQL database and inserts processed Spotify data from GetArtistsData if database does not exist."""
     connection = Database(SQL_DATA, print=True)
     connection.migrate()
-    connection.insert(INSERT_VALUES_INTO_TABLE, rows)
+    connection.insert(
+        """INSERT INTO 
+        top_artist_popularities (artist_name, popularity, term)
+        VALUES(?, ?, ?);""",
+        rows,
+    )
