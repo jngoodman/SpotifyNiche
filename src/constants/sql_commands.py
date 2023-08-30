4c08ddebc38633733c1a321174c8fcd1532cb5a7
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-30 22:40:43 +0100

    chore: remove unused constants

--- src/constants/sql_commands.py
+++ src/constants/sql_commands.py
@@ -3,27 +3,3 @@ INSERT INTO
 top_artist_popularities (artist_name, popularity, term)
 VALUES(?, ?, ?);
 """
-
-
-class EXTRACT_VALUES:
-    VALUES_INTRO = "SELECT * FROM top_artist_popularities WHERE"
-
-    VALUES_OUTRO = "ORDER BY popularity;"
-
-    AVG = """SELECT term, AVG(popularity)
-    FROM top_artist_popularities
-    GROUP BY term;"""
-
-    MOST = """SELECT term, artist_name, MAX(popularity)
-    FROM top_artist_popularities
-    GROUP BY term;"""
-
-    LEAST = """SELECT term, artist_name, MIN(popularity)
-    FROM top_artist_popularities
-    GROUP BY term;"""
-
-    EXTRACTION_KEYS = {
-        "short_term": "term IS 'short_term'",
-        "medium_term": "term IS 'medium_term'",
-        "long_term": "term IS 'long_term'",
-    }
