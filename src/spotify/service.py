8fc7b7d3815f22ff68166065e0691381356d4f73
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-31 19:58:42 +0100

    feat: implement equality methods

--- src/spotify/service.py
+++ src/spotify/service.py
@@ -17,12 +17,30 @@ class TopArtistsData:
     def from_dict(dict: dict):
         return TopArtistsData(name=dict["name"], popularity=dict["popularity"])
 
+    def __eq__(self, __value: "TopArtistsData") -> bool:
+        return __value.name == self.name and __value.popularity == self.popularity
+
 
 @dataclass
 class TopArtistsByTerm:
     SHORT: list[TopArtistsData]
     MEDIUM: list[TopArtistsData]
     LONG: list[TopArtistsData]
+
+    def __eq__(self, __value: "TopArtistsByTerm") -> bool:
+        for i in range(len(self.SHORT)):
+            if self.SHORT[i] != __value.SHORT[i]:
+                return False
+
+        for i in range(len(self.MEDIUM)):
+            if self.MEDIUM[i] != __value.MEDIUM[i]:
+                return False
+
+        for i in range(len(self.LONG)):
+            if self.LONG[i] != __value.LONG[i]:
+                return False
+
+        return True
 
 
 class SpotifyService:
@@ -33,9 +51,11 @@ class SpotifyService:
 
     def get_top_artists_by_terms(self):
         short_term_data = self._spotify.current_user_top_artists(time_range=TERMS.SHORT)
+
         medium_term_data = self._spotify.current_user_top_artists(
             time_range=TERMS.MEDIUM
         )
+
         long_term_data = self._spotify.current_user_top_artists(time_range=TERMS.LONG)
 
         top_artists_by_term = TopArtistsByTerm(
